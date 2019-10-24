38c167e12228e83c97a56f30b8135523d3bbb80f
Zavizenov <g_zavizenov@ats-hr420-dm22.fc.ca>
2019-10-24 12:36:59 -0400

    Initial commit to the repo

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 8889, // 8889 for mac, empty for windows
+    uname: "root",
+    pword: "root", // leave blank for windows
+    database: "db_card"
+}
+
+module.exports = config;
